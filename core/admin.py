1aad08d784531fd1a2c379f85d649d17fbf26e0d
Dilip <dilip.itsolutions4india@gmail.com>
2025-10-25 16:10:21 +0530

    first commit

---
+++ core/admin.py
@@ -0,0 +1,8 @@
+# No models in this example, but keep file for Django app completeness.
+from django.contrib import admin
+from .models import GoogleBotVisit
+
+@admin.register(GoogleBotVisit)
+class GoogleBotVisitAdmin(admin.ModelAdmin):
+    list_display = ('ip_address', 'path_accessed', 'timestamp')
+    search_fields = ('ip_address', 'user_agent')
