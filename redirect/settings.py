2fc156d5d316e2abc460d579dcf24f0c17986081
dilipitsolutions4india-cloud <dilip.itsolutions4india@gmail.com>
2025-10-31 13:39:27 +0530

    Updated static files

--- redirect/settings.py
+++ redirect/settings.py
@@ -78,3 +78,16 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For collectstatic
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
